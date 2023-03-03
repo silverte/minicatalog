8e639dd06f4637c42386764995ce6bf82889a062
silverte <km78cho@gmail.com>
2023-03-03 19:16:35 +0900

    modify buildspec.yaml

--- main/task.sh
+++ main/task.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws ecs register-task-definition --cli-input-json file://json/task-definition-planner.json;
-aws ecs register-task-definition --cli-input-json file://json/task-definition-manager.json;
+aws ecs register-task-definition --cli-input-json file://./main/json/task-definition-planner.json;
+aws ecs register-task-definition --cli-input-json file://./main/json/task-definition-manager.json;
